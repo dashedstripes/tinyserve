fa58d36ee55a59fcf3ada8ca1a3972ac0d86a169
Adam Gray <adamgrayscale@gmail.com>
2024-04-18 16:02:38 -0500

    add method

--- demo/app.ts
+++ demo/app.ts
@@ -20,7 +20,7 @@ app.get('/test', async (c) => {
   `)
 })
 
-app.get('/api', async (c) => {
+app.post('/api', async (c) => {
   return c.json({
     message: "Hello, World!"
   })
--- src/tinyserve.ts
+++ src/tinyserve.ts
@@ -1,10 +1,12 @@
 export class Context {
     request: Request
     url: string
+    method: string
 
     constructor(request: Request) {
         this.request = request
         this.url = new URL(request.url).pathname
+        this.method = request.method
     }
 
     html(html: string) {
@@ -26,30 +28,60 @@ export class Context {
     }
 }
 
-export class Tinyserve {
+export type RouteHandler = (c: Context) => Promise<Response>;
 
-    routes: { path: string, handler: (c: Context) => Promise<Response> }[]
+export class Router {
+    // method, path, handler
+    routes: [string, string, RouteHandler][] = []
 
     constructor() {
         this.routes = []
     }
 
-    get(path: string, handler: (c: Context) => Promise<Response>) {
-        this.routes.push({ path, handler })
+    add(method: string, path: string, handler: RouteHandler) {
+        this.routes.push([method, path, handler])
     }
 
-    async fetch(request: Request) {
-        const context = new Context(request)
-        return await this.handleRequest(context);
+    match(path: string) {
+        for (const route of this.routes) {
+            if (route[1] === path) {
+                return route
+            }
+        }
     }
 
     async handleRequest(c: Context) {
         const { url } = c
         for (const route of this.routes) {
-            if (route.path === url) {
-                return route.handler(c)
+            if (route[1] === url) {
+                if(route[0] !== c.method) {
+                    return new Response('Method Not Allowed', { status: 405 })
+                }
+
+                return route[2](c)
             }
         }
         return new Response('Not Found', { status: 404 })
+    }
+}
+
+export class Tinyserve {
+    router: Router;
+
+    constructor() {
+        this.router = new Router()
+    }
+
+    get(path: string, handler: RouteHandler) {
+        this.router.add("GET", path, handler)
+    }
+
+    post(path: string, handler: RouteHandler) {
+        this.router.add("POST", path, handler)
+    }
+
+    async fetch(request: Request) {
+        const context = new Context(request)
+        return await this.router.handleRequest(context);
     }
 }
